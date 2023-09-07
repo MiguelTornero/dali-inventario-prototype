99200b2fd03fa96e6d8168322ae1451f18b8fa9d
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2023-09-07 15:12:55 -0600

    added query function

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,7 +1,26 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+// Boilerplate so the db connection is saved between reloads
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+  };
+  
+  export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+      log:
+          process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    });
+  
+  if (process.env.NODE_ENV !== "production") { globalForPrisma.prisma = prisma; }
+// End of boilerplate
 
-async function checkConnection() {
-    
+export async function getItems(limit: number, skip: number, sort: ('name' | 'createdAt' | 'lastModified' | 'quantity' | 'lastCostCents') = 'name', desc = true) {
+    return prisma.item.findMany({
+        skip: skip,
+        take: limit,
+        orderBy: {
+          [sort]: desc ? 'desc' : 'asc'
+        }
+    })
 }
